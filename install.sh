8dcfb7a9a0edd439cb7821fe0d88960084cb8b22
John Maximilian <2e0byo@gmail.com>
2021-07-28 22:04:05 +0100

    use network install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-# hostname=$(cat ../.hostname)
-# password=$(cat ../.password)
-# mpfshell -o ws:$hostname,$password -n -c "putc $2"
+hostname=$(cat ../.hostname)
+password=$(cat ../.password)
+mpfshell -o ws:$hostname,$password -n -c "putc $2"
 port="/dev/ttyUSB0"
-mpfshell -o ser:$port -n -c "putc $2"
+# mpfshell -o ser:$port -n -c "putc $2"
 mkdir -p "$1"
 cp "$2" "$1"
 touch "updated"
