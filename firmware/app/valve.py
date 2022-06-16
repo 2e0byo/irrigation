c3cb41257acf32f3bde522bc7b4c334fbc7520a4
John Maximilian <2e0byo@gmail.com>
2022-06-16 11:49:33 +0100

    feat: new valve logic.
    Valve now has one async method for setting which waits or raises.

--- firmware/app/valve.py
+++ firmware/app/valve.py
@@ -10,6 +10,10 @@ else:
 from .settings import settings
 
 
+class ValveError(Exception):
+    """Unable to set valve."""
+
+
 class Valve:
     """A latching valve with no feedback."""
 
@@ -18,13 +22,15 @@ class Valve:
     OPENING = 2
     OPEN = 10
 
+    ATTEMPTS = 10
+
     def __init__(self, name, en, in1, in2):
         """Initialise a new valve() object."""
         self.en = en
         self.in1 = in1
         self.in2 = in2
         self.en.off()
-        self._state = False
+        self._state = self.CLOSED
         self.name = name
         self._logger = logging.getLogger(self.name)
 
@@ -45,20 +51,60 @@ class Valve:
         self.in2.on()
         self.en.on()
 
+    async def achieved_state(self, end_state):
+        """Determine if we have achieved the desired state."""
+        return True
+
     async def _pulse(self, end_state):
         on = end_state == self.OPEN
-        self._open() if on else self._close()
-        await asyncio.sleep(self.pulse_duration)
-        self.en.off()
-        self._state = end_state
-        self._logger.info(f"{'Opened' if on else 'Closed'} valve.")
+        for _ in range(self.ATTEMPTS):
+            self._open() if on else self._close()
+            await asyncio.sleep(self.pulse_duration)
+            self.en.off()
+            if await self.achieved_state(end_state):
+                self._state = end_state
+                self._logger.info(f"{'Opened' if on else 'Closed'} valve.")
+                return
+        raise ValveError(f"Failed to set valve in {self.ATTEMPTS} attempts!")
 
-    def state(self, val=None):
-        """Get or set the current valve state."""
-        if val:
-            asyncio.create_task(self._pulse(self.OPEN))
-            self._state = self.OPENING
-        elif val is False:
-            asyncio.create_task(self._pulse(self.CLOSED))
-            self._state = self.CLOSING
+    @property
+    def current_state(self):
+        """Get the current valve state."""
         return self._state
+
+    async def state(self, val: bool = None):
+        """
+        Get or set current valve state.
+
+        Args:
+            val[bool]: Set state (True is open).
+
+        Raises:
+            Exception if unable to achieve state.
+
+        """
+        if val:
+            self._state = self.OPENING
+            await self._pulse(self.OPEN)
+        elif val is False:
+            self._state = self.CLOSING
+            await self._pulse(self.CLOSED)
+        return self._state
+
+
+class RateAwareValve(Valve):
+    """A latching valve with feedback from a flowrate sensor."""
+
+    def __init__(self, *args, rate_callback, **kwargs):
+        """Initialise the RateAwareValve object."""
+        super().__init__(*args, **kwargs)
+        self.rate_callback = rate_callback
+        self.transition_seconds = 15
+
+    async def achieved_state(self, end_state):
+        """Determine from the flow rate if we have achieved the desired state."""
+        await asyncio.sleep(self.transition_seconds)
+        if end_state == self.CLOSED:
+            return self.rate_callback() == 0
+        else:
+            return self.rate_callback() > 0
--- firmware/tests/test_valve.py
+++ firmware/tests/test_valve.py
@@ -1,7 +1,7 @@
 import asyncio
 
 import pytest
-from app.valve import Valve
+from app.valve import RateAwareValve, Valve, ValveError
 
 
 @pytest.fixture
@@ -13,16 +13,100 @@ def valve(mocker):
 
 
 async def test_open_close(valve):
-    assert valve.state() == valve.CLOSED
-    valve.state(True)
-    assert valve.state() == valve.OPENING
+    assert valve.current_state == valve.CLOSED
+    asyncio.create_task(valve.state(True))
+    await asyncio.sleep(0)
+    assert valve.current_state == valve.OPENING
     await asyncio.sleep(0.3)
-    assert valve.state() == valve.OPEN
-    valve.state(False)
-    assert valve.state() == valve.CLOSING
+    assert valve.current_state == valve.OPEN
+    assert await valve.state() == valve.OPEN
+    asyncio.create_task(valve.state(False))
+    await asyncio.sleep(0)
+    assert valve.current_state == valve.CLOSING
     await asyncio.sleep(0.3)
-    assert valve.state() == valve.CLOSED
+    assert valve.current_state == valve.CLOSED
+    assert await valve.state() == valve.CLOSED
+
+
+async def test_fail_once(valve):
+    x = 0
+
+    async def fail_once(*args):
+        nonlocal x
+        x += 1
+        return x > 1
+
+    valve.achieved_state = fail_once
+    await valve.state(True)
+    assert x == 2
+    assert valve.current_state == valve.OPEN
+
+
+async def test_fail(valve):
+    async def state(*args):
+        return False
+
+    valve.achieved_state = state
+    with pytest.raises(ValveError):
+        await valve.state(True)
 
 
 async def test_pulse_duration(valve):
     assert valve.pulse_duration == 0.1
+
+
+@pytest.fixture
+def rate_valve(mocker):
+    v = RateAwareValve(
+        "RateValve",
+        mocker.Mock(),
+        mocker.Mock(),
+        mocker.Mock(),
+        rate_callback=mocker.Mock(),
+    )
+    settings = {"RateValve--pulse_duration": 0.1}
+    mocker.patch("app.valve.settings", settings)
+    v.transition_seconds = 0.5
+    return v
+
+
+async def test_rate_valve_on_off(rate_valve):
+    rate_valve.rate_callback.return_value = 10
+    await rate_valve.state(True)
+    assert rate_valve.current_state == rate_valve.OPEN
+    rate_valve.rate_callback.return_value = 0
+    await rate_valve.state(False)
+    assert rate_valve.current_state == rate_valve.CLOSED
+
+
+async def test_rate_valve_fail(rate_valve):
+    rate_valve.ATTEMPTS = 3
+    rate_valve.rate_callback.return_value = 10
+    with pytest.raises(ValveError):
+        await rate_valve.state(False)
+    assert rate_valve.current_state == rate_valve.CLOSING
+    rate_valve.rate_callback.return_value = 0
+    with pytest.raises(ValveError):
+        await rate_valve.state(True)
+    assert rate_valve.current_state == rate_valve.OPENING
+
+
+async def test_rate_valve_slow(rate_valve):
+    x = 0
+    start = 0
+    end = 10
+
+    def rate(*args):
+        nonlocal x, end
+        x += 1
+        return end if x > 1 else start
+
+    rate_valve.rate_callback = rate
+
+    await rate_valve.state(True)
+    assert x == 2
+    assert rate_valve.en.on.call_count == 2
+    start, end = end, start
+    x = 0
+    await rate_valve.state(False)
+    assert rate_valve.en.on.call_count == 4
